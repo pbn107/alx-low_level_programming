df0f02b17fbecb8f87dba26df31ffb74f7dbcd08
pbn107 <ndlovupb107>
2023-01-11 09:50:14 -0700

    update 0-create_array.c

---
+++ 0x0B-malloc_free/1-strup.c
@@ -0,0 +1,26 @@
+#include "main.h:"
+
+/**
+ * _strdup - check the code for ALX School students.
+ * @str: string
+ * Return: pointer to a string
+ */
+
+char *_strdup(char *str)
+{
+	int i = 0;
+	char *arr = malloc(sizeof(str) + 1);
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (str[i] != '\0')
+	{
+		arr[i] = str[i];
+		i++;
+	}
+	arr[i] = '\0';
+	return (arr);
+}
+
