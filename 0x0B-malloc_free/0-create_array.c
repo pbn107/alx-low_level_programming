0dca119293685c51b405c84fe8a06ec1179343ae
pbn107 <ndlovupb107>
2023-01-11 09:38:34 -0700

    update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,7 @@
 char *create_array(unsigned int size, char c)
 {
 	int i =0;
-	char *arr = malloc((size * sizeof(char) + 1);
+	char *arr = malloc((size * sizeof(char)) + 1);
 
 	if (size == 0)
 	{
