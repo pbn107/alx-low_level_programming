aa76c440acb367e825cd82b4c576ea939cc0f340
pbn107 <ndlovupb107>
2023-01-11 14:58:35 -0700

    add 3 files

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * main - check the code for ALX School students.
+ * _str_concat - check the code for ALX School students.
  * @s1: first string
- * @s2:second string
+ * @s2: second string
  * Return: pointer to a string.
  */
 char *str_concat(char *s1, char *s2)
 {
-	char * arr,*tmp = " "; 
+	char * arr,*tmp = " ";
 	int i = 0, j = 0;
 
 	if (s1 == NULL && s2 == NULL)
@@ -25,18 +25,18 @@ char *str_concat(char *s1, char *s2)
 	}
 	arr = malloc(sizeof(s1) + sizeof(s2) + 1);
 
-	while ( s1[i] != '\0')
+	while (s1[i] != '\0')
 	{
 		arr[i] = s1[i];
 		i++;
 	}
 
 
-	while ( s2[j] != '\0')
+	while (s2[j] != '\0')
 	{
-		arr[i+j] = s2[j];
+		arr[i + j] = s2[j];
 		j++;
 	}
-	arr[i+j] = '\0';
+	arr[i + j] = '\0';
 	return (arr);
 }
