1a32be2681ae9bdf55a78d604725fce0b7ec97e4
pbn107 <ndlovupb107>
2023-01-11 10:30:23 -0700

    update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -6,10 +6,22 @@
  * Return: pointer to a string
  */
 
+int len(char *s)
+{
+	int len = 0, i = 0;
+
+	while (s[i] != '\0')
+	{
+		len++;
+		i++;
+	}
+	return len;
+}
+
 char *_strdup(char *str)
 {
-	int i = 0;
-	char *arr = malloc(sizeof(str) + 1);
+	int i = 0, l = len(str);
+	char *arr = (char *)malloc(l * sizeof(char));
 
 	if (str == NULL)
 	{
