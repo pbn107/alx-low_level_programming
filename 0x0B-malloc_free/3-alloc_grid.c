b2d158ed2b3fb8196ba9062220d1bf8141ab389e
pbn107 <ndlovupb107>
2023-01-11 15:21:48 -0700

    add 3 files

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,14 +1,41 @@
 #include "main.h"
 
+/**
+ * alloc_grid - allocate memory
+ * @width: length of the inside array
+ * @height: length of the outside
+ * Return: number
+ */
 
-void free_grid(int **grid, int height)
+int **alloc_grid(int width, int height)
 {
-    int i = 0;
+    int i, j;
+    int **arr = (int**)malloc(sizeof(int*) * height);
+    
+    if( height <= 0 || width <= 0)
+    {
+        return NULL;
+    }
+    
+    if (arr == NULL)
+    {
+        return NULL;
+    }
+    
+    for (i = 0; i < height; i++)
+    {
+        arr[i] = (int*)malloc(sizeof(int) * width);
+    }
     
     for(i = 0; i < height; i++)
     {
-        free(grid[i]);
+        for(j= 0; j < width; j++)
+        {
+            arr[i][j] = 0;
+        }
     }
-    free(grid);
+    
+    return arr;
+    
 }
 
---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * free_grid - frees memory
+ * @grid: memory to be freed
+ * @height: height of the array
+ */
+
+void free_grid(int **grid, int height)
+{
+    int i = 0;
+    
+    for(i = 0; i < height; i++)
+    {
+        free(grid[i]);
+    }
+    free(grid);
+}
+
