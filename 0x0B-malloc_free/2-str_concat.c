6d927cf74cdebe290d387325d341179676f99a50
pbn107 <ndlovupb107>
2023-01-11 15:04:11 -0700

    add 3 files

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,11 +9,12 @@
 char *str_concat(char *s1, char *s2)
 {
 	char * arr,*tmp = " ";
+	char str[] = "";
 	int i = 0, j = 0;
 
 	if (s1 == NULL && s2 == NULL)
 	{
-		return (tmp);
+		return (str);
 	}
 	if (s1 == NULL)
 	{
