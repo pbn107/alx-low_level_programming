b5a24eaf80aae9ebe9f7eb6cf7990bffd0e30a0f
pbn107 <ndlovupb107>
2023-01-11 22:20:13 -0700

    update program arguments

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 
-int main(int argc, char *argv[])
+/**
+ * main - prints the name of the file
+ */
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("./%s\n", argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
