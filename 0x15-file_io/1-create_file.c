cd59a9e40a9b406fc28b5333d98f4c67af18b8ea
pbn107 <ndlovupb107>
2023-02-07 00:08:02 +0200

    create file I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,7 +3,7 @@
 /**
  * create_file - create a file
  * @filename: name of the file
- * @text_content
+ * @text_content: r=text to add
  * Return: integer
  */
 
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	fd = open(filename, O_CREAT |O_WRONLY|O_TRUNC);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC);
 	if (fd == -1)
 	{
 		return (-1);
