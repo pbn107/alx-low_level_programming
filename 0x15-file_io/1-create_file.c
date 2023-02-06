23f3cb39c402a7aa1fc5ed5168b0ee1210f8963d
pbn107 <ndlovupb107>
2023-02-07 00:05:32 +0200

    create file I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * create_file - create a file
+ * @filename: name of the file
+ * @text_content
+ * Return: integer
+ */
+
 int create_file(const char *filename, char *text_content)
 {
 	int fd, i = 0;
@@ -10,15 +17,16 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	if (!text_content)
-	{
-		return (-1);
-	}
 	fd = open(filename, O_CREAT |O_WRONLY|O_TRUNC);
-
 	if (fd == -1)
 	{
 		return (-1);
+	}
+
+	if (!text_content)
+	{
+		close(fd);
+		return (1);
 	}
 
 	while (text_content[i] != '\0')
@@ -30,6 +38,6 @@ int create_file(const char *filename, char *text_content)
 
 	c = (char)10;
 	write(fd, &c, sizeof(char));
-
+	close(fd);
 	return (1);
 }
