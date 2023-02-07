6677d58b844e2e2e8eaca9a360cbdad7c4a8a40c
pbn107 <ndlovupb107>
2023-02-07 12:12:35 +0200

    hot fix

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,7 +16,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		for (w = 0; text_content && text_content[w]; w++)
 			;
 		fd = open(filename, O_WRONLY | O_APPEND);
-		if (fd != -1 && text_content)
+		if (fd == -1 && text_content)
 		{
 			write(fd, text_content, w);
 			close(fd);
