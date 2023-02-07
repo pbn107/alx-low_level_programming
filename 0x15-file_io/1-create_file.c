b6db281ac5f0094a4589f730ce70b69d1c5c11a7
pbn107 <ndlovupb107>
2023-02-07 11:37:12 +0200

    hot fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,11 +18,11 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	wr= 0;
+	wr = 0;
 	while (text_content != NULL && text_content[wr] != '\0')
 	{
 		wr++;
-	}	
+	}
 	if (fd == -1)
 	{
 		return (-1);
@@ -33,7 +33,7 @@ int create_file(const char *filename, char *text_content)
 		text_content = "";
 	}
 
-	write(fd, text_content,wr);
+	write(fd, text_content, wr);
 	close(fd);
 	return (1);
 }
